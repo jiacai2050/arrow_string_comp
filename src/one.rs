80f46f2696a6ec952d20c5f5918a8162f727c075
jiacai2050 <jiacai2050@gmail.com>
2021-01-14 10:02:45 +0800

    add size comp

--- src/one.rs
+++ src/one.rs
@@ -7,8 +7,7 @@ const NUM_TAGS: usize = 20_000_000;
 use arrow::array::{Array, StringBuilder};
 use arrow::compute::kernels::comparison::neq_utf8_scalar;
 
-
-use std::time::Instant;
+use std::{mem, time::Instant};
 
 struct RAAITimer {
     start: std::time::Instant,
@@ -16,73 +15,91 @@ struct RAAITimer {
 
 impl Default for RAAITimer {
     fn default() -> Self {
-        Self { start: Instant::now() }
+        Self {
+            start: Instant::now(),
+        }
     }
 }
 
 impl RAAITimer {
-    fn done(self) -> String{
+    fn done(self) -> String {
         format!("{:?}", self.start.elapsed())
     }
 }
-
 
 pub fn example_with_vec() {
     println!("example_with_vec");
     let t = RAAITimer::default();
 
-    let string_vec: Vec<String> = (0..NUM_TAGS).enumerate()
+    let string_vec: Vec<String> = (0..NUM_TAGS)
+        .enumerate()
         .map(|(i, _)| {
             match i % 3 {
                 0 => "us-east",
                 1 => "us-midwest",
                 2 => "us-west",
                 _ => unreachable!(),
-            }.into()
+            }
+            .into()
         })
         .collect();
 
-     println!("created array with {} elements in {}", string_vec.len(), t.done());
-
+    let size: usize = string_vec
+        .iter()
+        .map(|s| s.len() + mem::size_of::<String>())
+        .sum();
+    println!(
+        "created array with {} elements in {}, size {}",
+        string_vec.len(),
+        t.done(),
+        size,
+    );
     // Boolean array of which values are in / not in us-west
     for _ in 0..10 {
         let t = RAAITimer::default();
-        let not_west_bitset: Vec<bool> = string_vec
-            .iter()
-            .map(|s| s != "us-west")
-            .collect();
+        let not_west_bitset: Vec<bool> = string_vec.iter().map(|s| s != "us-west").collect();
 
-        println!("Completed finding bitset: {} elements in {}", not_west_bitset.len(), t.done());
+        println!(
+            "Completed finding bitset: {} elements in {}",
+            not_west_bitset.len(),
+            t.done()
+        );
     }
 }
-
 
 pub fn example_with_arrow() {
     println!("example_with_arrow");
     let t = RAAITimer::default();
     let mut builder = StringBuilder::new(NUM_TAGS);
 
-    (0..NUM_TAGS)
-        .enumerate()
-        .for_each(|(i, _)| {
-            let location = match i % 3 {
-                0 => "us-east",
-                1 => "us-midwest",
-                2 => "us-west",
-                _ => unreachable!(),
-            };
+    (0..NUM_TAGS).enumerate().for_each(|(i, _)| {
+        let location = match i % 3 {
+            0 => "us-east",
+            1 => "us-midwest",
+            2 => "us-west",
+            _ => unreachable!(),
+        };
 
-            builder.append_value(location).unwrap()
-        });
+        builder.append_value(location).unwrap()
+    });
 
     let array = builder.finish();
-
-    println!("created array with {} elements in {}", array.len(), t.done());
+    let size = array.get_array_memory_size();
+    println!(
+        "created array with {} elements in {}, size {}",
+        array.len(),
+        t.done(),
+        size
+    );
 
     // Boolean array of which values are in / not in us-west
     for _ in 0..10 {
         let t = RAAITimer::default();
         let not_west_bitset = neq_utf8_scalar(&array, "us-west").unwrap();
-        println!("Found {} not in west in {}", not_west_bitset.len(), t.done());
+        println!(
+            "Found {} not in west in {}",
+            not_west_bitset.len(),
+            t.done()
+        );
     }
 }
